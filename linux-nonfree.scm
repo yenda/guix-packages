4cf0581f6a3147a0760d1cd9066d60e701d23608
Eric Dvorsak <eric@dvorsak.fr>
2015-08-12 19:08:55 +0200

    fix linux-non-free path

--- linux-nonfree.scm
+++ linux-nonfree.scm
@@ -23,7 +23,7 @@
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
 
-(define-module (gnu packages linux-nonfree)
+(define-module (linux-nonfree)
   #:use-module ((guix licenses) #:hide (zlib))
   #:use-module (gnu packages linux)
   #:use-module (guix build-system trivial)
